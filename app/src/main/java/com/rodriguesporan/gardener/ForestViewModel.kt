47d7d11534ef70b064bdf5112fffa8b39c1ac2ad
rodriguesporan <rodriguesporan@gmail.com>
2022-02-22 20:55:37 -0300

    added destinations to navigation graph

---
+++ app/src/main/java/com/rodriguesporan/gardener/ForestViewModel.kt
@@ -0,0 +1,7 @@
+package com.rodriguesporan.gardener
+
+import androidx.lifecycle.ViewModel
+
+class ForestViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
--- app/src/main/java/com/rodriguesporan/gardener/ui/MainActivity.kt
+++ app/src/main/java/com/rodriguesporan/gardener/ui/MainActivity.kt
@@ -9,7 +9,6 @@ import com.rodriguesporan.gardener.R
 class MainActivity : AppCompatActivity() {
 
     private val tabLayout by lazy { findViewById<TabLayout>(R.id.tab_layout) }
-    private val tvContent by lazy { findViewById<TextView>(R.id.text_view_content) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,10 +29,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setTextToContentTextViewByTabPosition(tabPosition: Int?) {
-        tvContent.text = when (tabPosition) {
-            0 -> resources.getString(R.string.my_garden)
-            1 -> resources.getString(R.string.forest)
-            else -> resources.getString(R.string.main_activity)
-        }
+//        tvContent.text = when (tabPosition) {
+//            0 -> resources.getString(R.string.my_garden)
+//            1 -> resources.getString(R.string.forest)
+//            else -> resources.getString(R.string.main_activity)
+//        }
     }
 }
